3d3d759cedc613baea851f5902f4afcccebca04b
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-08-21 21:06:28 -0300

    :books: docs: update serializers

--- product/serializer/product_serializer.py
+++ product/serializer/product_serializer.py
@@ -1,12 +1,31 @@
 from rest_framework import serializers
-
-from product.models.product import Product
-from product.serializer.category_serializer import CategorySerializer
+from product.models import Category, Product
+from product.serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(required=True, many=True)
+    # 'category' para leitura, exibe detalhes das categorias associadas
+    category = CategorySerializer(read_only=True, many=True)
+    # 'categories_id' para escrita, utiliza chaves primárias das categorias
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        write_only=True,
+        many=True
+    )
 
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'active', 'categories']
+        fields = ['id', 'title', 'description', 'price', 'active', 'category', 'categories_id']
 
+    def create(self, validated_data):
+        # Remove 'categories_id' dos dados validados
+        category_data = validated_data.pop('categories_id', [])
+        # Cria o objeto Product com os dados restantes
+        product = Product.objects.create(**validated_data)
+
+        # Se houver dados de categorias, associá-las ao produto
+        if category_data:
+            product.category.set(category_data)
+        else:
+            raise serializers.ValidationError("At least one category must be provided.")
+
+        return product
