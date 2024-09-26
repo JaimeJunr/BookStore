1fb187118d60a580a4c64d7aed9a5e826a8df688
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-09-26 16:36:15 -0300

    add hello world and update server rotes

--- app/bookStore/settings.py
+++ app/bookStore/settings.py
@@ -74,7 +74,7 @@ if DEBUG:
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,"bookstore", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- app/bookStore/urls.py
+++ app/bookStore/urls.py
@@ -20,10 +20,14 @@ from django.urls import path, re_path, include
 import debug_toolbar
 from rest_framework.authtoken.views import obtain_auth_token
 
+from bookStore import views
+
 urlpatterns = [
     path("__debug__", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_auth_token"),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
