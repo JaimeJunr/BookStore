b905b34e7504934a24c14fe7ad0f4cbea5bbfd12
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-09-28 01:39:57 -0300

    fix

--- app/bookStore/views.py
+++ app/bookStore/views.py
@@ -13,9 +13,8 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo('app/bookStore/views.py')
+        repo = git.Repo('/home/jaimejunr/BookStore')
         origin = repo.remotes.origin
-
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
     else:
